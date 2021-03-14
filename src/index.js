2e521e321113ea419df3752ebdd8df90ecf41518
Linar <>
2021-03-14 19:11:19 +0300

    feat: complete towel-sort algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+      return [];
+  }
+  var myArr = [];
+  for (let i = 0; i < matrix.length; i ++) {
+      if (i % 2 === 0) {
+          for (let j = 0; j < matrix[i].length; j++) {
+              myArr.push(matrix[i][j]);
+          }
+      }
+      else {
+          for (let j = matrix[i].length - 1; j >= 0; j--) {
+              myArr.push(matrix[i][j]);
+          }
+      }
+  }
+  return myArr;
 }
